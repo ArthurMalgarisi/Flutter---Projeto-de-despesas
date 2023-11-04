24977325099b40784e89dfe26cf332656d334969
ArthurMalgarisi <arthurmalgarisi@gmail.com>
2023-11-04 16:10:05 -0300

    v 1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-
 import 'package:flutter/material.dart';
+import 'models/transaction.dart';
 
-main() => runApp(ExpensesApp());
+main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-
+  const ExpensesApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: MyHomePage());
@@ -12,28 +12,47 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Despesas Pessoais'),
+        title: const Text('Despesas Pessoais'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
-            width: double.infinity,
             child: Card(
               color: Colors.blue,
               child: Text('Gráfico'),
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text('Lista de Transações'),
-          ),
+          Column(
+              children: _transactions.map((tr) {
+            return Card(
+              child: Text(tr.title),
+            );
+          }).toList())
         ],
-      )
+      ),
     );
   }
 }
