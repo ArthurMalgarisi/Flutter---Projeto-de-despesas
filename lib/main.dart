98a521c408024c946b85f4c25b707a13c2a43fc2
ArthurMalgarisi <arthurmalgarisi@gmail.com>
2023-11-04 14:28:38 -0300

    Projeto de despesas

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+          ),
+        ],
+      )
+    );
+  }
+}
